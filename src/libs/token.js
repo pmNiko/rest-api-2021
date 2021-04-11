95794c937165a730acd31110c7c01840ebc038a2
pmNiko <nikolas090189@gmail.com>
2021-04-11 12:09:22 -0300

    Config environments

--- src/libs/token.js
+++ src/libs/token.js
@@ -6,12 +6,12 @@ import path from "path";
 import fs from "fs";
 
 // import private and public key
-const publicKey = fs.readFileSync(path.join(__dirname, "../keys/public.pem"));
-const privateKey = fs.readFileSync(path.join(__dirname, "../keys/private.pem"));
+const key = fs.readFileSync(path.join(__dirname, "../keys/private.pem"));
+const cert = fs.readFileSync(path.join(__dirname, "../keys/public.pem"));
 const options = { expiresIn: "6h", algorithm: "RS256" };
 
 // create token with private key
-export const sign = (payload) => jwt.sign(payload, privateKey, options);
+export const sign = (payload) => jwt.sign(payload, key, options);
 
 // verify token with public key
-export const verify = (token) => jwt.verify(token, publicKey);
+export const verify = (token) => jwt.verify(token, cert);
--- src/server/database.js
+++ src/server/database.js
@@ -6,16 +6,27 @@ import dotenv from "dotenv";
 
 const { NODE_ENV } = process.env;
 const result = dotenv.config();
-const { DB_HOST, DB_PORT, DB_DATABASE, DB_DATABASE_TEST } = result.parsed;
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_DATABASE_DEV,
+  DB_DATABASE_TEST,
+} = result.parsed;
 
-let environment = `${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+const url = `${DB_HOST}:${DB_PORT}/`;
+let environment = "";
 
-// if (NODE_ENV === "prod") {
-//   environment = `${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-// }
-
-if (NODE_ENV === "test") {
-  environment = `${DB_HOST}:${DB_PORT}/${DB_DATABASE_TEST}`;
+switch (NODE_ENV) {
+  case "prod":
+    environment = `${url}${DB_DATABASE}`;
+    break;
+  case "dev":
+    environment = `${url}${DB_DATABASE_DEV}`;
+    break;
+  default:
+    environment = `${url}${DB_DATABASE_TEST}`;
+    break;
 }
 
 export async function connect() {
